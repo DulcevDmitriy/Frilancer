cbbdf939597514ee8fa049e51a5edad41ad3680a
Dulcev Dmitriy <ddvvvvvv@yandex.ru>
2023-03-24 15:17:43 +0300

    frilTestEdit

--- src/main/java/ru/netology/services/IncomeService.java
+++ src/main/java/ru/netology/services/IncomeService.java
@@ -3,12 +3,12 @@ package ru.netology.services;
 public class IncomeService {
 
     public int calculate(int income, int expenses) {
-        int count = 0; // счётчик месяцев отдыха
+        int count = 1; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
-        for (int month = 0; month < 12; month++) {
-            if (money >= expenses) { // можем ли отдыхать?
+        for (int month = 1; month < 12; month++) {
+            if (money >= expenses * 3) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = money - expenses;
+                money = (money - expenses);
             } else {
                 money = money + income;
             }
